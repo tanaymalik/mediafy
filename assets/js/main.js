b1c82783c6764c5fa199d5eef6b9f73d08d8ae46
tanaymalik <tanaymalik06@gmail.com>
2024-04-20 18:26:20 +0530

    create home page on new branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,3 @@
-/*=============== SHOW MENU ===============*/
-const headerToggle = document.getElementById('header-toggle'),
-      main = document.getElementById('main'),
-      navClose = document.getElementById('nav-close')
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(headerToggle){
-    headerToggle.addEventListener('click', () =>{
-        main.classList.add('show-menu')
-    })
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        main.classList.remove('show-menu')
-    })
-}
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const main = document.getElementById('main')
-    // When we click on each nav__link, we remove the show-menu class
-    main.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
@@ -56,3 +18,6 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+
